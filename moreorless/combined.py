6927b38b6cf6f9b76de6d5fc426f7372e29b8163
Tim Hatch <tim@timhatch.com>
2025-04-04 10:09:13 -0700

    Unify combined diff code :)

--- moreorless/combined.py
+++ moreorless/combined.py
@@ -7,15 +7,54 @@ but in a nutshell, lines are preceeded by more than one +/-/space symbol.
 These are useful for understanding merges, divergence from a common ancestor,
 or when you expect one diff and got another (to avoid displaying a
 diff-of-diff, which is hard for humans to parse).
+
+Confusing:
+
+    $ diff -u <( diff -u a b ) <( diff -u a c)
+    --- /dev/fd/11	2025-04-03 13:46:26
+    +++ /dev/fd/12	2025-04-03 13:46:26
+    @@ -1,6 +1,6 @@
+     --- a	2025-04-03 13:12:09
+    -+++ b	2025-04-03 13:35:12
+    ++++ c	2025-04-03 13:12:27
+     @@ -1,2 +1,2 @@
+      def main():
+     -    pass
+    -+    print("b")
+    ++    print("hi")
+
+Better:
+
+    $ python -m moreorless.combined a b c
+    --- a/file
+    +++ b/file
+    +++ b/file
+      def main():
+    --    pass
+    +     print("b")
+     +    print("hi")
+
+
+Another confusing example from writing tests for a thing that produces diffs:
+
+    AssertionError: '--- a/file\n--- a/file\n+++ b/file\n- a\n -b\n' != '--- a/file\n+++ b/file\n+++ b/file\n- a\n -b\n'
+      --- a/file
+    - --- a/file
+    + +++ b/file
+      +++ b/file
+      - a
+       -b
+
 """
 
 from difflib import SequenceMatcher
-from typing import Dict, List, Sequence, Tuple
+from typing import Dict, List, Optional, Sequence, Tuple
 
 
 def _line_symbols(
     files: Sequence[str],
     common: str,
+    merge: bool,
 ) -> List[Tuple[str, ...]]:
     dest_lines = common.splitlines()
     # (file_symbol, ...)
@@ -30,6 +69,13 @@ def _line_symbols(
             inserted_lines[dest_idx][s] = {}
         return inserted_lines[dest_idx][s]
 
+    if merge:
+        old = "-"
+        new = "+"
+    else:
+        old = "+"
+        new = "-"
+
     for si, src in enumerate(files):
         src_lines = src.splitlines()
         for tag, i1, i2, j1, j2 in SequenceMatcher(
@@ -37,9 +83,9 @@ def _line_symbols(
         ).get_opcodes():
             for i in range(i1, i2):
                 if tag == "delete":
-                    helper(src_lines[i], j1)[si] = "-"
+                    helper(src_lines[i], j1)[si] = old
                 elif tag == "replace":
-                    helper(src_lines[i], j1)[si] = "-"
+                    helper(src_lines[i], j1)[si] = old
                 else:
                     assert tag == "equal"
 
@@ -47,9 +93,9 @@ def _line_symbols(
                 if tag == "equal":
                     dest_line_symbols[i].append(" ")
                 elif tag == "insert":
-                    dest_line_symbols[i].append("+")
+                    dest_line_symbols[i].append(new)
                 elif tag == "replace":
-                    dest_line_symbols[i].append("+")
+                    dest_line_symbols[i].append(new)
                 else:  # pragma: no cover
                     raise AssertionError(tag)
 
@@ -57,52 +103,64 @@ def _line_symbols(
 
     result = []
     for i in range(len(dest_lines) + 1):
+        if not merge:
+            if i < len(dest_lines):
+                result.append((dest_lines[i], *dest_line_symbols[i]))
+
         for t, symbols in inserted_lines[i].items():
             lst = [" "] * len(files)
             for a, b in symbols.items():
                 lst[a] = b
             result.append((t, *lst))
-        if i < len(dest_lines):
-            result.append((dest_lines[i], *dest_line_symbols[i]))
+
+        if merge:
+            if i < len(dest_lines):
+                result.append((dest_lines[i], *dest_line_symbols[i]))
     return result
 
 
-FLIP_TABLE = {
-    ord("-"): "+",
-    ord("+"): "-",
-}
-
-
-def divergent_diff(
-    original: str,
+def combined_diff(
     files: Sequence[str],
-    filename: str = "file",
+    basename: str = "file",
+    filenames: Optional[Sequence] = None,
+    merge: bool = False,
 ) -> str:
     """
     Returns a combined unified diff of the changes turning `original` into each of `files`.
     """
-    buf: List[str] = []
-    buf.append(f"--- a/{filename}\n")
-    for _ in range(len(files)):
-        buf.append(f"+++ b/{filename}\n")
-    for text, *symbols in _line_symbols(files, common=original):
-        # flip +/-
-        buf.append("".join(symbols).translate(FLIP_TABLE) + text + "\n")
-    return "".join(buf)
+    if not filenames:
+        if not merge:
+            filenames = [f"a/{basename}"]
+            filenames.extend([f"b/{basename}"] * (len(files) - 1))
+        else:
+            filenames = [f"a/{basename}"] * (len(files) - 1)
+            filenames.append(f"b/{basename}")
+    assert len(filenames) == len(files), filenames
 
-
-def merge_diff(
-    files: Sequence[str],
-    final: str,
-    filename: str = "file",
-) -> str:
-    """
-    Returns a combined unified diff of the changes incorporating `files` heads to result in `final`.
-    """
     buf: List[str] = []
-    for _ in range(len(files)):
-        buf.append(f"--- a/{filename}\n")
-    buf.append(f"+++ b/{filename}\n")
-    for text, *symbols in _line_symbols(files, common=final):
+    if not merge:
+        buf.append(f"--- {filenames[0]}\n")
+        for i in range(1, len(files)):
+            buf.append(f"+++ {filenames[i]}\n")
+    else:
+        for i in range(0, len(files) - 1):
+            buf.append(f"--- {filenames[i]}\n")
+        buf.append(f"+++ {filenames[-1]}\n")
+
+    if merge:
+        common = files.pop(-1)
+    else:
+        common = files.pop(0)
+
+    for text, *symbols in _line_symbols(files, common=common, merge=merge):
         buf.append("".join(symbols) + text + "\n")
+
     return "".join(buf)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    import sys
+    from pathlib import Path
+
+    lst = [Path(f).read_text() for f in sys.argv[1:]]
+    print(combined_diff(lst, merge=False))
--- moreorless/tests/combined.py
+++ moreorless/tests/combined.py
@@ -1,9 +1,120 @@
 from unittest import TestCase
 
-from moreorless.combined import _line_symbols, divergent_diff, merge_diff
+from moreorless.combined import _line_symbols, combined_diff
 
 
-class CombinedTest(TestCase):
+class DivergentDiffTest(TestCase):
+    def test_basic(self) -> None:
+        result = combined_diff(["", "a\n", "b\n"], merge=False)
+        self.assertEqual(
+            """\
+--- a/file
++++ b/file
++++ b/file
++ a
+ +b
+""",
+            result,
+        )
+
+    def test_basic_with_explicit_names(self) -> None:
+        result = combined_diff(
+            ["", "a\n", "b\n"], filenames=["x", "y", "z"], merge=False
+        )
+        self.assertEqual(
+            """\
+--- x
++++ y
++++ z
++ a
+ +b
+""",
+            result,
+        )
+
+    def test_two(self) -> None:
+        self.assertEqual(
+            [
+                ("a", " "),
+                ("b", "-"),
+                ("c", "-"),
+            ],
+            _line_symbols(["a\n"], common="a\nb\nc\n", merge=False),
+        )
+
+    def test_two_removal(self) -> None:
+        self.assertEqual(
+            [
+                ("a", " "),
+                ("b", "+"),
+            ],
+            _line_symbols(["a\nb\n"], common="a\n", merge=False),
+        )
+
+    def test_two_replace(self) -> None:
+        self.assertEqual(
+            [
+                ("x", "-"),
+                ("a", "+"),
+                ("b", " "),
+            ],
+            _line_symbols(["a\nb\n"], common="x\nb\n", merge=False),
+        )
+
+    def test_three(self) -> None:
+        self.assertEqual(
+            [
+                #     a    ab
+                ("a", " ", " "),
+                ("b", "-", " "),
+                ("c", "-", "-"),
+            ],
+            _line_symbols(["a\n", "a\nb\n"], common="a\nb\nc\n", merge=False),
+        )
+
+    def test_three_insert_same(self) -> None:
+        # this covers the path inside `helper` when we already have a matching line
+        self.assertEqual(
+            [
+                #     abc  abd
+                ("a", " ", " "),
+                ("c", " ", "-"),
+                ("b", "+", "+"),
+                ("d", " ", "+"),
+            ],
+            _line_symbols(["a\nb\nc\n", "a\nb\nd\n"], common="a\nc\n", merge=False),
+        )
+
+
+class MergeDiffTest(TestCase):
+    def test_basic(self) -> None:
+        result = combined_diff(["a\n", "b\n", ""], merge=True)
+        self.assertEqual(
+            """\
+--- a/file
+--- a/file
++++ b/file
+- a
+ -b
+""",
+            result,
+        )
+
+    def test_basic_with_explicit_names(self) -> None:
+        result = combined_diff(
+            ["a\n", "b\n", ""], filenames=["x", "y", "z"], merge=True
+        )
+        self.assertEqual(
+            """\
+--- x
+--- y
++++ z
+- a
+ -b
+""",
+            result,
+        )
+
     def test_two(self) -> None:
         self.assertEqual(
             [
@@ -11,7 +122,7 @@ class CombinedTest(TestCase):
                 ("b", "+"),
                 ("c", "+"),
             ],
-            _line_symbols(["a\n"], "a\nb\nc\n"),
+            _line_symbols(["a\n"], common="a\nb\nc\n", merge=True),
         )
 
     def test_two_removal(self) -> None:
@@ -20,7 +131,7 @@ class CombinedTest(TestCase):
                 ("a", " "),
                 ("b", "-"),
             ],
-            _line_symbols(["a\nb\n"], "a\n"),
+            _line_symbols(["a\nb\n"], common="a\n", merge=True),
         )
 
     def test_two_replace(self) -> None:
@@ -30,7 +141,7 @@ class CombinedTest(TestCase):
                 ("x", "+"),
                 ("b", " "),
             ],
-            _line_symbols(["a\nb\n"], "x\nb\n"),
+            _line_symbols(["a\nb\n"], common="x\nb\n", merge=True),
         )
 
     def test_three(self) -> None:
@@ -41,7 +152,7 @@ class CombinedTest(TestCase):
                 ("b", "+", " "),
                 ("c", "+", "+"),
             ],
-            _line_symbols(["a\n", "a\nb\n"], "a\nb\nc\n"),
+            _line_symbols(["a\n", "a\nb\n"], common="a\nb\nc\n", merge=True),
         )
 
     def test_three_insert_same(self) -> None:
@@ -54,35 +165,5 @@ class CombinedTest(TestCase):
                 ("d", " ", "-"),
                 ("c", " ", "+"),
             ],
-            _line_symbols(["a\nb\nc\n", "a\nb\nd\n"], "a\nc\n"),
-        )
-
-
-class DivergentDiffTest(TestCase):
-    def test_basic(self) -> None:
-        result = divergent_diff("", ["a\n", "b\n"])
-        self.assertEqual(
-            """\
---- a/file
-+++ b/file
-+++ b/file
-+ a
- +b
-""",
-            result,
-        )
-
-
-class MergeDiffTest(TestCase):
-    def test_basic(self) -> None:
-        result = merge_diff(["a\n", "b\n"], "")
-        self.assertEqual(
-            """\
---- a/file
---- a/file
-+++ b/file
-- a
- -b
-""",
-            result,
+            _line_symbols(["a\nb\nc\n", "a\nb\nd\n"], common="a\nc\n", merge=True),
         )
